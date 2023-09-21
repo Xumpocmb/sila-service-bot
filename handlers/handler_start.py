bb9eb0d9cb39a4e87eaf9e982875e34c4fb3a7cd
Xumpocmb <x.xumpocmb@gmail.com>
2023-09-21 17:48:20 +0300

    work with other db-s

--- handlers/handler_start.py
+++ handlers/handler_start.py
@@ -12,7 +12,9 @@ router: Router = Router()
 
 @router.message(Command('start'))
 async def cmd_admin_start(message: Message):
-    await message.answer(f'Hello, {message.from_user.username}!\n'
-                         'Выбери пункт из меню:', reply_markup=main_menu_keyboard)
+    await message.answer(f'Доброго времени суток, мой юный падаван, {message.from_user.username}!\n'
+                         f'Добро пожаловать в мир услуг Электросилы\n'
+                         'Выбери нужный раздел и читай описание/наполнение каждой услуги', reply_markup=main_menu_keyboard)
     # await cmd_start_db(message)
     await message.delete()
+
