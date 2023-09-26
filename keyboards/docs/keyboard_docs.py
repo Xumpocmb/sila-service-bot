587a2a3d1d18d1c5be468c798c38cc726813d8c4
Xumpocmb <x.xumpocmb@gmail.com>
2023-09-26 17:50:18 +0300

    docs

--- keyboards/docs/keyboard_docs.py
+++ keyboards/docs/keyboard_docs.py
@@ -6,7 +6,7 @@ from aiogram.types import (
 button_1: InlineKeyboardButton = InlineKeyboardButton(text='ГС', callback_data='doc-gs')
 button_2: InlineKeyboardButton = InlineKeyboardButton(text='Профилактика', callback_data='doc-profilaktica')
 button_3: InlineKeyboardButton = InlineKeyboardButton(text='Доп. Сервис', callback_data='doc-additional')
-button_4: InlineKeyboardButton = InlineKeyboardButton(text='Доп. Сервис', callback_data='doc-podryada')
+button_4: InlineKeyboardButton = InlineKeyboardButton(text='Установка', callback_data='doc-podryada')
 button_5: InlineKeyboardButton = InlineKeyboardButton(text='<< Назад', callback_data='main_menu')
 
 
